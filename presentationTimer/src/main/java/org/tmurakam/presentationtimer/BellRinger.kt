92949b56e9ed4b037408ee561808f677ca49c965
Takuya Murakami <tmurakam@tmurakam.org>
2022-04-23 19:09:20 +0900

    feat: Convert BellRinger to kotlin

--- presentationTimer/src/main/java/org/tmurakam/presentationtimer/BellRinger.kt
+++ presentationTimer/src/main/java/org/tmurakam/presentationtimer/BellRinger.kt
@@ -1,43 +1,42 @@
-package org.tmurakam.presentationtimer;
+package org.tmurakam.presentationtimer
 
-import android.content.Context;
-import android.media.MediaPlayer;
-import android.os.Vibrator;
+import android.content.Context
+import android.media.MediaPlayer
+import android.os.Vibrator
 
 /**
  * ベル音・バイブレータ再生
  */
-public class BellRinger {
-    /** 音声データ */
-    private MediaPlayer[] mBells;
+class BellRinger(context: Context) {
+    /** 音声データ  */
+    private val mBells: Array<MediaPlayer> = arrayOf(
+        MediaPlayer.create(context, R.raw.bell1),
+        MediaPlayer.create(context, R.raw.bell2),
+        MediaPlayer.create(context, R.raw.bell3),
+    )
 
-    /** バイブレータ */
-    private Vibrator mVibrator;
-    private long[][] mVibratorPattern;
+    /** バイブレータ  */
+    private val mVibrator: Vibrator
+    private val mVibratorPattern = arrayOf(
+        longArrayOf(0, 500),
+        longArrayOf(0, 500, 200, 500),
+        longArrayOf(0, 500, 200, 500, 200, 500)
+    )
 
     /**
      * コンストラクタ
-     * @param context
      */
-    public BellRinger(Context context) {
-        mBells = new MediaPlayer[3];
-        mBells[0] = MediaPlayer.create(context, R.raw.bell1);
-        mBells[1] = MediaPlayer.create(context, R.raw.bell2);
-        mBells[2] = MediaPlayer.create(context, R.raw.bell3);
-        for (MediaPlayer p : mBells) {
-            p.setVolume(1.0f, 1.0f);
+    init {
+        for (p in mBells) {
+            p.setVolume(1.0f, 1.0f)
         }
 
-        mVibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
-        mVibratorPattern = new long[3][];
-        mVibratorPattern[0] = new long[]{0, 500};
-        mVibratorPattern[1] = new long[]{0, 500, 200, 500};
-        mVibratorPattern[2] = new long[]{0, 500, 200, 500, 200, 500};
+        mVibrator = context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
     }
 
-    public void release() {
-        for (MediaPlayer p : mBells) {
-            p.release();
+    fun release() {
+        for (p in mBells) {
+            p.release()
         }
     }
 
@@ -45,16 +44,16 @@ public class BellRinger {
      * Bell鳴動
      * @param n ベル番号(0-2)
      */
-    public void ringBell(int n) {
-        mBells[n].seekTo(0);
-        mBells[n].start();
+    fun ringBell(n: Int) {
+        mBells[n].seekTo(0)
+        mBells[n].start()
     }
 
     /**
      * バイブレータ起動
      * @param n ベル番号(0-2)
      */
-    public void vibrate(int n) {
-        mVibrator.vibrate(mVibratorPattern[n], -1);
+    fun vibrate(n: Int) {
+        mVibrator.vibrate(mVibratorPattern[n], -1)
     }
 }
