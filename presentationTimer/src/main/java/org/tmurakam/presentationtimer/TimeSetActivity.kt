51b87104eef6cfa5413bd1322390c39695d58d2d
Takuya Murakami <tmurakam@tmurakam.org>
2022-04-30 11:58:13 +0900

    feat: Convert TimeSetActivity to Kotlin

--- presentationTimer/src/main/java/org/tmurakam/presentationtimer/TimeSetActivity.kt
+++ presentationTimer/src/main/java/org/tmurakam/presentationtimer/TimeSetActivity.kt
@@ -1,64 +1,58 @@
+package org.tmurakam.presentationtimer
 
-package org.tmurakam.presentationtimer;
-
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.CheckBox;
-
-import com.ikovac.timepickerwithseconds.view.TimePicker;
+import android.app.Activity
+import android.os.Bundle
+import android.view.View
+import android.widget.CheckBox
+import com.ikovac.timepickerwithseconds.view.TimePicker
 
 /**
  * 時刻設定 Activity
  */
-public class TimeSetActivity extends Activity {
-    private int mKind;
+class TimeSetActivity : Activity() {
+    private var mKind = 0
+    private var mTimePicker: TimePicker? = null
+    private var mCheckIsEndTime: CheckBox? = null
+    private var mPrefs: Prefs? = null
 
-    private TimePicker mTimePicker;
-
-    private CheckBox mCheckIsEndTime;
-
-    private Prefs mPrefs;
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
         //setTheme(R.style.MyDialog);
-        setContentView(R.layout.time_set);
+        setContentView(R.layout.time_set)
 
-        mTimePicker = (TimePicker)findViewById(R.id.TimePicker);
-        mCheckIsEndTime = (CheckBox)findViewById(R.id.CheckUseAsEndTime);
+        mTimePicker = findViewById(R.id.TimePicker)
+        mCheckIsEndTime = findViewById(R.id.CheckUseAsEndTime)
 
-        mKind = getIntent().getIntExtra("kind", 1);
+        mKind = intent.getIntExtra("kind", 1)
 
-        mPrefs = new Prefs(this);
-        int time = mPrefs.getBellTime(mKind);
-        int hour = time / 3600;
-        int min = (time / 60) % 60;
-        int sec = time % 60;
+        mPrefs = Prefs(this)
+        val time = mPrefs!!.getBellTime(mKind)
+        val hour = time / 3600
+        val min = time / 60 % 60
+        val sec = time % 60
 
-        mTimePicker.setIs24HourView(true);
-        mTimePicker.setCurrentHour(hour);
-        mTimePicker.setCurrentMinute(min);
-        mTimePicker.setCurrentSecond(sec);
+        mTimePicker?.setIs24HourView(true)
+        mTimePicker?.currentHour = hour
+        mTimePicker?.currentMinute = min
+        mTimePicker?.setCurrentSecond(sec)
 
-        mCheckIsEndTime.setChecked(mKind == mPrefs.getCountDownTarget());
+        mCheckIsEndTime?.isChecked = mKind == mPrefs!!.countDownTarget
     }
 
-    public void onClickOk(View v) {
-        int hour = mTimePicker.getCurrentHour();
-        int min = mTimePicker.getCurrentMinute();
-        int sec = mTimePicker.getCurrentSeconds();
+    fun onClickOk(v: View?) {
+        val hour = mTimePicker!!.currentHour
+        val min = mTimePicker!!.currentMinute
+        val sec = mTimePicker!!.currentSeconds
 
-        mPrefs.setBellTime(mKind, hour * 3600 + min * 60 + sec);
-        if (mCheckIsEndTime.isChecked()) {
-            mPrefs.setCountDownTarget(mKind);
+        mPrefs?.setBellTime(mKind, hour * 3600 + min * 60 + sec)
+        if (mCheckIsEndTime!!.isChecked) {
+            mPrefs?.countDownTarget = mKind
         }
-        finish();
+        finish()
     }
 
-    public void onClickCancel(View v) {
-        finish();
+    fun onClickCancel(v: View?) {
+        finish()
     }
 }
