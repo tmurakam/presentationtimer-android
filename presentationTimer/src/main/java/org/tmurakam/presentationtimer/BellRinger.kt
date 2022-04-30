aa7a8bf40fd9c608779bd29bd3dc0a4e393aef1a
Takuya Murakami <tmurakam@tmurakam.org>
2022-04-30 13:09:19 +0900

    refactor: Remove deprecated api call for vibration

--- presentationTimer/src/main/java/org/tmurakam/presentationtimer/BellRinger.kt
+++ presentationTimer/src/main/java/org/tmurakam/presentationtimer/BellRinger.kt
@@ -2,7 +2,10 @@ package org.tmurakam.presentationtimer
 
 import android.content.Context
 import android.media.MediaPlayer
+import android.os.Build
+import android.os.VibrationEffect
 import android.os.Vibrator
+import android.os.VibratorManager
 
 /**
  * ベル音・バイブレータ再生
@@ -31,7 +34,13 @@ class BellRinger(context: Context) {
             p.setVolume(1.0f, 1.0f)
         }
 
-        mVibrator = context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+        mVibrator = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            val vibratorManager = context.getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
+            vibratorManager.defaultVibrator
+        } else {
+            @Suppress("DEPRECATION")
+            context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+        }
     }
 
     fun release() {
@@ -54,6 +63,13 @@ class BellRinger(context: Context) {
      * @param n ベル番号(0-2)
      */
     fun vibrate(n: Int) {
-        mVibrator.vibrate(mVibratorPattern[n], -1)
+        val pattern = mVibratorPattern[n]
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            mVibrator.vibrate(VibrationEffect.createWaveform(pattern, -1));
+        } else {
+            @Suppress("DEPRECATION")
+            mVibrator.vibrate(pattern, -1)
+        }
     }
 }
